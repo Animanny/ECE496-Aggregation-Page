e02ccc004612b64e7d566719667560d828262b65
Ani Srikanth <ani.srikanth23@gmail.com>
2022-12-01 00:42:55 -0500

    Add navbar buttons (not real buttons)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
-      <Navbar header={"Assignment Script Configurator"}></Navbar>
+      <Navbar header={"Capstone"}></Navbar>
       <Body></Body>
     </div>
   );
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import "./css/navbar.css";
+import logo from "./static/capstone.png";
 
 export default class Navbar extends Component {
   constructor(props) {
@@ -12,7 +13,10 @@ export default class Navbar extends Component {
   render() {
     return (
       <div class="NavBar">
+        <img src={logo} alt="Logo"></img>
         <h1>{this.state.header}</h1>
+        <h5>{"Assignment Performance"}</h5>
+        <h5>{"Script Configurator"}</h5>
       </div>
     );
   }
